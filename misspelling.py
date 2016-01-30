3b29e746bf75a96ba25b0e7d85066c10a4eb9154
JackPotte <hubert.bossot@gmail.com>
2016-01-30 15:35:14 +0100

    txt

--- misspelling.py
+++ misspelling.py
@@ -20,7 +20,7 @@ Command line options:
    -main       only check pages in the main namespace, not in the talk,
                wikipedia, user, etc. namespaces.
 """
-__version__ = '$Id: 75171ba7dc42466ba2c59519ec3f52cba3804446 $'
+__version__ = '$Id$'
 
 # (C) Daniel Herding, 2007
 # (C) Pywikipedia bot team 2007-2013
