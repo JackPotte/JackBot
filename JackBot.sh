a97492c1b622e92935d3c2494d37ca0a7d57e2e5
JackPotte <hubert.bossot@gmail.com>
2017-07-01 17:31:27 +0200

    New sandboxes

--- JackBot.sh
+++ JackBot.sh
@@ -1,12 +1,20 @@
 # Native
-#python core/pwb.py replace      -lang:commons -family:commons -file:articles_commons.txt "[[Category:PDF Wikibooks]]" "[[Category:English Wikibooks PDF]]"
-#python core/pwb.py touch        -lang:fr -family:wiktionary -cat:"Singuliers manquants en anglais"
-#python core/pwb.py movepages    -lang:fr -family:wiktionary -pairs:"articles_fr_wiktionary.txt" -noredirect -pairs
-#python core/pwb.py protect      -lang:fr -family:wiktionary -cat:"Élections de patrouilleurs" -summary:"Vote archivé" -move:sysop -edit:sysop
-#python core/pwb.py delete       -lang:fr -family:wikiversity -file:"scripts/JackBot/articles_fr_wiktionary.txt"
+#python core/pwb.py replace       -lang:commons -family:commons -file:articles_commons.txt "[[Category:PDF Wikibooks]]" "[[Category:English Wikibooks PDF]]"
+#python core/pwb.py touch         -lang:fr -family:wiktionary -cat:"Singuliers manquants en anglais"
+#python core/pwb.py movepages     -lang:fr -family:wiktionary -pairs:"articles_fr_wiktionary.txt" -noredirect -pairs
+#python core/pwb.py protect       -lang:fr -family:wiktionary -cat:"Élections de patrouilleurs" -summary:"Vote archivé" -move:sysop -edit:sysop
+#python core/pwb.py delete        -lang:fr -family:wikiversity -file:"scripts/JackBot/articles_fr_wiktionary.txt"
+python core/pwb.py clean_sandbox -lang:fr -family:wiktionary  -always
+python core/pwb.py clean_sandbox -lang:fr -family:wikibooks   -always
+python core/pwb.py clean_sandbox -lang:fr -family:wikinews    -always
+python core/pwb.py clean_sandbox -lang:fr -family:wikiversity -always
+python core/pwb.py clean_sandbox -lang:fr -family:wikisource  -always
+python core/pwb.py clean_sandbox -lang:fr -family:wikiquote   -always
+python core/pwb.py clean_sandbox -lang:fr -family:wikivoyage  -always
+#python core/pwb.py clean_sandbox -lang:fr -family:wikipedia   -always
 
 # Homemade
-python core/pwb.py src/fr.wiktionary.format
+#python core/pwb.py src/fr.wiktionary.format
 #python core/pwb.py src/fr.wikipedia.format
 #python core/pwb.py src/fr.wikinews.2wikipedia
 #python core/pwb.py src/fr.wikiquote.count-quotes
