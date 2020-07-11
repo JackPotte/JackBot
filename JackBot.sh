e53633221a83ea12fa7c2e534d84910552b9b817
JackPotte <hubert.bossot@gmail.com>
2020-07-11 12:41:20 +0200

    + login all

--- JackBot.sh
+++ JackBot.sh
@@ -1,4 +1,4 @@
-#*** Native ***
+#== Native scripts ==
 #python core/pwb.py replace       -lang:commons -family:commons -file:articles_commons_txt "[[Category:PDF Wikibooks]]" "[[Category:English Wikibooks PDF]]"
 #python core/pwb.py movepages     -lang:fr -family:wiktionary -pairsfile:"articles_fr_wiktionary.txt" -noredirect
 #python core/pwb.py protect       -lang:fr -family:wiktionary -cat:"Élections de patrouilleurs" -summary:"Vote archivé" -move:sysop -edit:sysop
@@ -7,7 +7,9 @@
 #python core/pwb.py touch         -lang:fr -family:wiktionary -cat:"Pluriels manquants en français" -namespace:0
 #python core/pwb.py touch         -lang:fr -family:wiktionary -cat:"Singuliers manquants en anglais" -namespace:0
 #python core/pwb.py touch         -lang:en -family:wikibooks -transcludes:"Template:Qr-em" -namespace:0
+#python core/pwb.py login -all
 
+#=== Periodic tasks ===
 #python core/pwb.py clean_sandbox -lang:fr -family:wiktionary  -always
 #python core/pwb.py clean_sandbox -lang:fr -family:wikibooks   -always
 #python core/pwb.py clean_sandbox -lang:fr -family:wikinews    -always
@@ -24,7 +26,7 @@
 #python core/pwb.py redirect double -always -family:wikipedia -lang:se
 #python core/pwb.py redirect double -always -family:wikipedia -lang:tr
 #python core/pwb.py redirect double -always -family:wikipedia -lang:ta
-#
+
 #python core/pwb.py redirect double -always -family:wiktionary -lang:de
 #python core/pwb.py redirect double -always -family:wiktionary -lang:en
 #python core/pwb.py redirect double -always -family:wiktionary -lang:es
@@ -35,7 +37,7 @@
 #python core/pwb.py redirect double -always -family:wiktionary -lang:ru
 #python core/pwb.py redirect double -always -family:wiktionary -lang:sv
 #python core/pwb.py redirect double -always -family:wiktionary -lang:zh
-#
+
 #python core/pwb.py redirect double -always -family:wikiversity -lang:en
 #python core/pwb.py redirect double -always -family:wikiversity -lang:fr
 #python core/pwb.py redirect double -always -family:wikibooks -lang:en
@@ -48,18 +50,18 @@
 #python core/pwb.py redirect double -always -family:wikinews -lang:fr
 #python core/pwb.py redirect double -always -family:wikivoyage -lang:de
 #python core/pwb.py redirect double -always -family:wikivoyage -lang:fr
-#
+
 #python core/pwb.py redirect broken -lang:fr -family:wiktionary -always
 #python core/pwb.py redirect broken -lang:fr -family:wikiversity -always
 ##python core/pwb.py redirect broken -lang:fr -family:wikibooks -always # pb du livre de photos
 
 
-#*** Maintained ***
+#== Maintained scripts ==
 #python core/pwb.py src/fr_wikinews_to_wikipedia
 #python core/pwb.py src/fr_wikiquote_count_quotes -family:wikiquote -output:"User:JackBot/statistiques" -outputquotes:"Template:NUMBEROFQUOTES"
 
 
-#*** Homemade ***
+#== Homemade scripts ==
 #python core/pwb.py src/fr_wikibooks_format -cat
 #python core/pwb.py src/fr_wikinews_format -cat
 #python core/pwb.py src/fr_wikiquote_format -cat
@@ -73,7 +75,7 @@
 #python core/pwb.py src/wiktionary/fr_wiktionary_format -cat
 #python core/pwb.py src/wiktionary/fr_wiktionary_format_py -u Escarbot revocation 1000
 
-# Current daily global operations
+#=== Current daily global operations ===
 python core/pwb.py src/en_wikibooks_format -nocat
 python core/pwb.py src/fr_wikibooks_format -nocat
 python core/pwb.py src/fr_wikiversity_format -nocat
