3a4a98cb421e91a81e28380b84c8b99c4c3fa1bd
hbossot <hbossot@profideo.com>
2022-11-19 14:15:25 +0100

    Fix touch.sh

--- devops/touch.sh
+++ devops/touch.sh
@@ -3,7 +3,7 @@
 cd JackBot
 devops/update_JackBot.sh
 
-python3 core/pwb.py touch -lang:fr -family:wikibooks -page:Main_Page --purge
+python3 core/pwb.py touch -lang:fr -family:wikibooks -page:Accueil --purge
 python3 core/pwb.py touch -lang:en -family:wikibooks -transcludes:"Template:Qr-em" -namespace:0
 
 python3 core/pwb.py touch -lang:fr -family:wiktionary -cat:"Pluriels manquants en français" -namespace:0
