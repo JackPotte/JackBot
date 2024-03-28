af4df65cfc16e875900a37d23203f890e3593c3c
Hubert BOSSOT <hbossot@homeland.immo>
2024-03-28 22:04:25 +0100

    Archive [[Wiktionnaire:Boîte à idées]]

--- src/wiktionary/fr_wiktionary_archive.py
+++ src/wiktionary/fr_wiktionary_archive.py
@@ -41,7 +41,6 @@ for debug_alias in debug_aliases:
 site_language, site_family, site = get_site_by_file_name(__file__)
 username = config.usernames[site_family][site_language]
 
-summary = 'Autoarchivage de [[Wiktionnaire:Bot/Requêtes]]'
 closed_status = [
     'fait',
     'refus',
@@ -63,7 +62,7 @@ def treat_page_by_name(page_name, waiting_time_before_archiving=3):
     Ne pas archiver tout de suite si page très récemment modifiée
     TODO (Idéalement, la date des dernières interventions devrait être checkée...)
     '''
-    latest_rev = page.editTime()
+    latest_rev = page.latest_revision.timestamp
     now = datetime.datetime.now()
     inactivity_duration = (now - latest_rev).days
     if wait_after_humans and inactivity_duration < waiting_time_before_archiving:
@@ -80,6 +79,8 @@ def treat_page_by_name(page_name, waiting_time_before_archiving=3):
             print(' Untreated namespace')
         return
 
+    summary = 'Autoarchivage de [[' + page_name + ']]'
+
     try:
         page_content = page.get()
     except pywikibot.exceptions.NoPageError as e:
@@ -88,8 +89,8 @@ def treat_page_by_name(page_name, waiting_time_before_archiving=3):
     except pywikibot.exceptions.IsRedirectPageError as e:
         print(e)
         page_content = page.get(get_redirect=True)
-        page2 = Page(site, page_content[page_content.find(
-            '[[')+2:page_content.find(']]')])
+        page2 = Page(site, page_content[page_content.find('[[')+2:page_content.find(']]')])
+
         try:
             final_page_content2 = page2.get()
         except pywikibot.exceptions.NoPageError as e:
@@ -101,13 +102,15 @@ def treat_page_by_name(page_name, waiting_time_before_archiving=3):
         except pywikibot.exceptions.LockedPageError as e:
             print(e)
             return
-        if final_page_content2.find('{{NavigBOT') == -1:
+
+        if 'Bots' in page_name and '{{NavigBOT' not in final_page_content2:
             final_page_content2 = (
                 '{{NavigBOT|'
                 + page2.title()[len(page2.title()) - 4 :]
                 + '}}\n'
                 + final_page_content2
             )
+
             save_page(page2, final_page_content2, summary)
         return
     except pywikibot.exceptions.LockedPageError as e:
@@ -118,56 +121,59 @@ def treat_page_by_name(page_name, waiting_time_before_archiving=3):
 
     # TODO utiliser get_section() pour éviter les balises imbriquées : (ou juste pre, nowiki, et source qui peuvent contenir "==").
     # ex : https://fr.wiktionary.org/w/index.php?title=Wiktionnaire:Bots/Requ%C3%AAtes&diff=prev&oldid=29274173
-    regex = r'\n==[ ]*{{[rR]equête [fait|refus|refusée|sans suite]+}}.*==[ \t]*\n'
+    regex = r'\n===?[ ]*{{[rR]equête [fait|refus|refusée|sans suite]+}}.*===?[ \t]*\n'
     final_page_content = ''
     while re.compile(regex).search(page_content):
-        debut_paragraphe = re.search(regex, page_content).end()
-        if re.search(r'\n==[^=]', page_content[debut_paragraphe:]):
-            fin_paragraphe = re.search(
-                r'\n==[^=]', page_content[debut_paragraphe:]).start()
+        paragraph_start = re.search(regex, page_content).end()
+        if re.search(r'\n===?[^=]', page_content[paragraph_start:]):
+            paragraph_end = re.search(r'\n===?[^=]', page_content[paragraph_start:]).start()
         else:
-            fin_paragraphe = len(page_content[debut_paragraphe:])
-        if debug_level > 0:
-            input(page_content[debut_paragraphe:][:fin_paragraphe])
-            print('-------------------------------------')
-        if page_content[debut_paragraphe:].find('\n==') == -1:
-            # Dernier paragraphe
-            final_page_content += page_content[:debut_paragraphe][
-                page_content[:debut_paragraphe].rfind('\n=='):] + page_content[debut_paragraphe:]
-            page_content = page_content[:debut_paragraphe][:page_content[:debut_paragraphe].rfind(
-                '\n==')]
-        else:
-            final_page_content += page_content[:debut_paragraphe][
-                page_content[:debut_paragraphe].rfind('\n=='):] + page_content[debut_paragraphe:][
-                :fin_paragraphe]
-            page_content = page_content[:debut_paragraphe][
-                :page_content[:debut_paragraphe].rfind('\n==')] + page_content[debut_paragraphe:][
-                fin_paragraphe:]
+            paragraph_end = len(page_content[paragraph_start:])
 
-    if page_content != page.get():
-        page2 = Page(site, f'{page_name}/Archives/{current_year}')
-        final_page_content2 = ''
-        if page2.exists():
-            try:
-                final_page_content2 = page2.get()
-            except pywikibot.exceptions.NoPageError as e:
-                print(e)
-                return
-            except pywikibot.exceptions.IsRedirectPageError as e:
-                print(e)
-                return
-            except pywikibot.exceptions.LockedPageError as e:
-                print(e)
-                return
-        if final_page_content2.find('{{NavigBOT') == -1:
-            final_page_content2 = (
-                '{{NavigBOT|'
-                + page2.title()[len(page2.title()) - 4 :]
-                + '}}\n'
-                + final_page_content2
-            )
-        save_page(page2, final_page_content2 + final_page_content, summary)
-        save_page(page, page_content, summary)
+        if debug_level > 0:
+            input(page_content[paragraph_start:][:paragraph_end])
+            print('-------------------------------------')
+
+        if page_content[paragraph_start:].find('\n==') == -1:
+            # Last paragraph
+            final_page_content += (page_content[:paragraph_start][page_content[:paragraph_start].rfind('\n=='):]
+                                   + page_content[paragraph_start:])
+            page_content = page_content[:paragraph_start][:page_content[:paragraph_start].rfind('\n==')]
+        else:
+            final_page_content += (page_content[:paragraph_start][page_content[:paragraph_start].rfind('\n=='):]
+                                   + page_content[paragraph_start:][:paragraph_end])
+            page_content = (page_content[:paragraph_start][:page_content[:paragraph_start].rfind('\n==')]
+                            + page_content[paragraph_start:][paragraph_end:])
+
+    if page_content == page.get():
+        if debug_level > 0:
+            print(' No change')
+        return
+
+    page2 = Page(site, f'{page_name}/Archives/{current_year}')
+    final_page_content2 = ''
+    if page2.exists():
+        try:
+            final_page_content2 = page2.get()
+        except pywikibot.exceptions.NoPageError as e:
+            print(e)
+            return
+        except pywikibot.exceptions.IsRedirectPageError as e:
+            print(e)
+            return
+        except pywikibot.exceptions.LockedPageError as e:
+            print(e)
+            return
+
+    if 'Bots' in page_name and '{{NavigBOT' not in final_page_content2:
+        final_page_content2 = (
+            '{{NavigBOT|'
+            + page2.title()[len(page2.title()) - 4 :]
+            + '}}\n'
+            + final_page_content2
+        )
+    save_page(page2, final_page_content2 + final_page_content, summary)
+    save_page(page, page_content, summary)
 
 
 def main(*args) -> int:
@@ -177,6 +183,7 @@ def main(*args) -> int:
             wait_after_humans = False
             treat_page_by_name(f'User:{username}/test unitaire')
     else:
+        treat_page_by_name('Wiktionnaire:Boîte à idées')
         treat_page_by_name('Wiktionnaire:Bots/Requêtes')
     return 0
 
